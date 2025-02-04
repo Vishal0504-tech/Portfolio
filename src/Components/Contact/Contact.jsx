7ef0be87fe00bbf8b0e4e740f4b0c15dadc823ab
Vishal0504-tech <vrvishal0504@gmail.com>
2025-02-04 14:47:26 +0530

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       id="Contact"
       className="flex justify-around  bg-[#0e0f10] text-white  md:p-20 sm:p-20  p-20 items-center"
     >
-      <div className="mr-10">
+      <div className="ml-10">
         <h1 className="text-2xl md:text-6xl font-bold">Contact</h1>
         <h3 className="text-sm md:text-2xl pr-2 font-normal">
           Feel Free To reach out!
